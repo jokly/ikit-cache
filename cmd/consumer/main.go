80891bdaae264c27a0fefe0170511c67f107e54b
Tikhon Slasten <slasten32@gmail.com>
2021-03-18 09:05:43 +1000

    Implement request in consumer

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"ikit-cache/internal/transport/proto"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
 
 func main() {
-	fmt.Println("consumer")
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := proto.NewRandomServiceClient(conn)
+
+	request(client)
+}
+
+func request(c proto.RandomServiceClient) {
+	ctx := context.Background()
+	req := &proto.GetRandomDataStreamRequest{}
+
+	stream, err := c.GetRandomDataStream(ctx, req)
+	if err != nil {
+		log.Printf("couldn't get stream: %v\n", err)
+		return
+	}
+
+	resp, err := stream.Recv()
+	if err != nil {
+		log.Printf("couldn't get response: %v\n", err)
+		return
+	}
+
+	log.Println(resp.Result)
 }
